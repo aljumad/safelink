fe426c02d9446e14a7deff1299c37f3abd979301
Ali Aljumad <41227671+aljumad@users.noreply.github.com>
2019-02-04 09:30:40 +0800

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://linktogetmyfiles.blogspot.com/2015/03/get.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://linktogetmyfiles.blogspot.com/2015/03/get.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
